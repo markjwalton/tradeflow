5e9050539c84cf53fee2799f7515e7d3c093629e
Mark Walton <mark.walton@gmail.com>
2025-12-03 08:37:58 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -84,7 +84,7 @@ const globalAdminPages = [
 ];
 
 // Pages that don't require tenant context but need to preserve query params
-const standalonePages = ["CMSManager", "APIManager", "SecurityMonitor", "PerformanceMonitor", "RoadmapManager", "RoadmapJournal", "SprintManager", "RuleBook", "PlaygroundSummary", "PlaygroundEntity", "PlaygroundPage", "PlaygroundFeature", "ConceptWorkbench", "TestDataManager", "LivePreview", "MindMapEditor", "GeneratedApps", "EntityLibrary", "PageLibrary", "FeatureLibrary", "PackageLibrary", "TenantManager", "BusinessTemplates", "WorkflowLibrary", "WorkflowDesigner", "FormTemplates", "FormBuilder", "ChecklistTemplates", "ChecklistBuilder", "PromptSettings", "LookupTestForms", "CommunityLibrary", "CommunityPublish"];
+const standalonePages = ["DashboardManager", "CMSManager", "APIManager", "SecurityMonitor", "PerformanceMonitor", "RoadmapManager", "RoadmapJournal", "SprintManager", "RuleBook", "PlaygroundSummary", "PlaygroundEntity", "PlaygroundPage", "PlaygroundFeature", "ConceptWorkbench", "TestDataManager", "LivePreview", "MindMapEditor", "GeneratedApps", "EntityLibrary", "PageLibrary", "FeatureLibrary", "PackageLibrary", "TenantManager", "BusinessTemplates", "WorkflowLibrary", "WorkflowDesigner", "FormTemplates", "FormBuilder", "ChecklistTemplates", "ChecklistBuilder", "PromptSettings", "LookupTestForms", "CommunityLibrary", "CommunityPublish"];
 
 // Pages that render their own navigation (no layout sidebar)
 const fullscreenPages = ["LivePreview"];
