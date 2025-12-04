9143586e2725f473c0ac073bf0f949ea906d1c53
Mark Walton <mark.walton@gmail.com>
2025-12-04 07:28:28 +0000

    File changes

--- src/pages/ComponentShowcase.jsx
+++ src/pages/ComponentShowcase.jsx
@@ -557,10 +557,10 @@ export default function ComponentShowcase() {
               action={<PrimaryButton><Plus className="h-4 w-4 mr-2" />Create Item</PrimaryButton>}
             />
           </ComponentDemo>
-        </ShowcaseSection>
+        </ShowcaseSection>}
 
         {/* Navigation Section */}
-        <ShowcaseSection
+        {visibleCategories.includes("navigation") && <ShowcaseSection
           id="navigation"
           title="Navigation"
           count={8}
