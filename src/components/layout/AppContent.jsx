3232ae8fec2bffdcad01dd4e2f136cb6b62b3aa9
Mark Walton <mark.walton@gmail.com>
2025-12-09 08:40:52 +0000

    File changes

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -3,16 +3,12 @@ import { useLocation } from "react-router-dom";
 
 export function AppContent({ children, navItems = [] }) {
   const location = useLocation();
-  const currentPage = location.pathname.split("/").pop();
-  const showBreadcrumbs = currentPage !== ""; // Always show breadcrumbs except empty path
 
   return (
     <main className="flex-1 overflow-y-auto">
-      {showBreadcrumbs && (
-        <div className="px-4 pt-4 pb-2 md:px-8">
-          <AppBreadcrumb organizedNavigation={navItems} />
-        </div>
-      )}
+      <div className="px-4 pt-4 pb-2 md:px-8">
+        <AppBreadcrumb organizedNavigation={navItems} />
+      </div>
       <div className="px-4 py-6 md:px-8 md:py-8">
         {children}
       </div>
