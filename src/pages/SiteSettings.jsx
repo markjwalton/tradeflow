aa9acc204b494296817ac0e766b81d5195b83a84
Mark Walton <mark.walton@gmail.com>
2025-12-09 15:19:13 +0000

    File changes

--- src/pages/SiteSettings.jsx
+++ src/pages/SiteSettings.jsx
@@ -166,21 +166,21 @@ export default function SiteSettings() {
                 >
                   <SelectTrigger id="background-color" className="flex-1">
                     <SelectValue />
-                    </SelectTrigger>
-                    <SelectContent>
+                  </SelectTrigger>
+                  <SelectContent>
                     {colorTokens.map((color) => (
                       <SelectItem key={color.value} value={color.value}>
                         {color.label}
                       </SelectItem>
                     ))}
-                    </SelectContent>
-                    </Select>
-                    </div>
-                    </div>
-                    </CardContent>
-                    </Card>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
 
-                    <Card>
+        <Card>
           <CardHeader>
             <CardTitle>Overlay Settings</CardTitle>
             <CardDescription>Configure the overlay appearance for modals and panels</CardDescription>
