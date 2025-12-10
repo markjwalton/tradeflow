e92466ff85e47abd4a0de073f6c6d39eed754b37
Mark Walton <mark.walton@gmail.com>
2025-12-10 00:45:04 +0000

    File changes

--- src/components/navigation/GenericNavEditor.jsx
+++ src/components/navigation/GenericNavEditor.jsx
@@ -407,7 +407,7 @@ export default function GenericNavEditor({
 
   return (
     <>
-    <Card>
+    <Card className="rounded-xl">
       {title && (
         <CardHeader className="flex flex-row items-center justify-between">
           <CardTitle>{title}</CardTitle>
