ff55eb649b61d6bcc2f9c9734cde6b6cfb84517c
Mark Walton <mark.walton@gmail.com>
2025-12-13 21:33:32 +0000

    File changes

--- src/components/ui/tabs.jsx
+++ src/components/ui/tabs.jsx
@@ -10,7 +10,7 @@ const TabsList = React.forwardRef(({ className, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground",
+      "inline-flex h-9 items-center justify-center rounded-lg bg-white/50 p-1 text-muted-foreground",
       className
     )}
     {...props}
