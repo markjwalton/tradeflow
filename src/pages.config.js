4cbc5d951383eb60c31ab7402147f44a75a43a68
Mark Walton <mark.walton@gmail.com>
2025-12-14 02:36:57 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,3 +1,6 @@
 // Auto-generated pages configuration
 // This file is used by the navigation system to track available pages
-export const pagesConfig = {};
+export const pagesConfig = {
+  pages: [],
+  routes: {}
+};
