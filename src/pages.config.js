aab10e0c82789dfe39885253033e575452bc115f
Mark Walton <mark.walton@gmail.com>
2025-12-14 02:38:47 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,10 +1,12 @@
 // Auto-generated pages configuration
 // This file is used by the navigation system to track available pages
 
-export const pagesConfig = {
-  pages: [],
-  routes: {},
-  metadata: {}
-};
+export const pagesConfig = Object.freeze({
+  pages: Object.freeze([]),
+  routes: Object.freeze({}),
+  metadata: Object.freeze({}),
+  navigationItems: Object.freeze([]),
+  pageComponents: Object.freeze({})
+});
 
 export default pagesConfig;
