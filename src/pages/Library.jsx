e18739507f10f295168e55dd2e7680d3f1d438ec
Mark Walton <mark.walton@gmail.com>
2025-12-14 19:08:30 +0000

    File changes

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -3,8 +3,6 @@ import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { createPageUrl } from "@/utils";
-import { useNavigate } from "react-router-dom";
-import { createPageUrl } from "@/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
