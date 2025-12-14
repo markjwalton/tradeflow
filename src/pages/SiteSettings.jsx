ff2c8c1eca47274a99d9e4499152ac3fa71a8fa4
Mark Walton <mark.walton@gmail.com>
2025-12-14 08:19:57 +0000

    File changes

--- src/pages/SiteSettings.jsx
+++ src/pages/SiteSettings.jsx
@@ -218,7 +218,7 @@ export default function SiteSettings() {
   };
 
   return (
-    <div className="container mx-auto pb-8 px-4 max-w-4xl">
+    <div className="container mx-auto pb-8 px-4 max-w-4xl -mt-6">
       <PageHeader 
         title="Site Settings"
         description="Configure global appearance settings for the application"
