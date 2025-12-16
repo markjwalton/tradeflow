1cbbfadaf4e812855b8829c0e0af19f9b8dba17a
Mark Walton <mark.walton@gmail.com>
2025-12-16 07:57:28 +0000

    File changes

--- src/pages/StandardPageReference.jsx
+++ src/pages/StandardPageReference.jsx
@@ -444,7 +444,6 @@ export default function StandardPageReference() {
             {/* Footer - Full Width */}
             <footer className="bg-white border-t border-gray-200 mt-auto">
               <div className="px-4 sm:px-6 lg:px-8 py-12 md:flex md:items-center md:justify-between">
-          <div className="px-4 sm:px-6 lg:px-8 py-12 md:flex md:items-center md:justify-between">
                 <div className="flex justify-center gap-x-6 md:order-2">
                   {[
                     {
