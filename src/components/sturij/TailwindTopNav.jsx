ecd708bb2ea0f47ee3be8a5c7dddde89c7c5e79c
Mark Walton <mark.walton@gmail.com>
2025-12-21 01:40:16 +0000

    File changes

--- src/components/sturij/TailwindTopNav.jsx
+++ src/components/sturij/TailwindTopNav.jsx
@@ -14,6 +14,7 @@ export default function TailwindTopNav({
   user = null,
   onSearch,
   onNotificationClick,
+  onMobileMenuClick,
 }) {
   return (
     <Disclosure as="header" className="relative z-10 bg-white shadow-sm">
@@ -39,12 +40,15 @@ export default function TailwindTopNav({
             </div>
           </div>
           <div className="relative z-10 flex items-center lg:hidden">
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-600">
+            <button
+              type="button"
+              onClick={onMobileMenuClick}
+              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-600"
+            >
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open menu</span>
-              <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
-              <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
-            </DisclosureButton>
+              <Bars3Icon aria-hidden="true" className="size-6" />
+            </button>
           </div>
           <div className="hidden lg:relative lg:z-10 lg:ml-4 lg:flex lg:items-center">
             <button
